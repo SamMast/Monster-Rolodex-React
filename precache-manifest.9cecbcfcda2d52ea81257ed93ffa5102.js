b0df807831388263614ff03b4c645822c52c7670
Samuel Mast <samuel.a.mast@gmail.com>
2019-11-04 22:26:27 -0700

    Updates

---
+++ precache-manifest.9cecbcfcda2d52ea81257ed93ffa5102.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7d8fd653b8f3461cfa1c2cfcbbf00b",
+    "url": "/Monster-Rolodex-React/index.html"
+  },
+  {
+    "revision": "418a859b3807c117fe7b",
+    "url": "/Monster-Rolodex-React/static/css/main.1deeda70.chunk.css"
+  },
+  {
+    "revision": "b5533f3f6b664c822b95",
+    "url": "/Monster-Rolodex-React/static/js/2.dbf58ef4.chunk.js"
+  },
+  {
+    "revision": "418a859b3807c117fe7b",
+    "url": "/Monster-Rolodex-React/static/js/main.6b01b30a.chunk.js"
+  },
+  {
+    "revision": "a97608108fd5d63b9fcf",
+    "url": "/Monster-Rolodex-React/static/js/runtime-main.75751dbc.js"
+  }
+]);
